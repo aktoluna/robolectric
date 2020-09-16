e3dbbc24f2e174b521af5d664a7f65cba6652cd8
Googler <noreply@google.com>
2020-09-16 15:09:58 -0700

    Add setDisplayHdrCapabilities to ShadowDisplay.
    PiperOrigin-RevId: 332098376

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowDisplay.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowDisplay.java
@@ -7,8 +7,11 @@ import static org.robolectric.util.reflector.Reflector.reflector;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.Display.HdrCapabilities;
 import android.view.Surface;
 import android.view.WindowManager;
 import org.robolectric.RuntimeEnvironment;
@@ -397,6 +400,30 @@ public class ShadowDisplay {
       ShadowDisplayManager.changeDisplay(realObject.getDisplayId(),
           di -> di.state = state);
     }
+  }
+
+  /**
+   * Set HDR capabilities to the display sourced with displayId. see {@link HdrCapabilities} for
+   * supportedHdrTypes.
+   */
+  public void setDisplayHdrCapabilities(
+      int displayId,
+      float maxLuminance,
+      float maxAverageLuminance,
+      float minLuminance,
+      int... supportedHdrTypes) {
+    if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+      // HDR capabilities are not supported below Android N.
+      return;
+    }
+
+    ShadowDisplayManager.changeDisplay(
+        displayId,
+        displayConfig -> {
+          displayConfig.hdrCapabilities =
+              new HdrCapabilities(
+                  supportedHdrTypes, maxLuminance, maxAverageLuminance, minLuminance);
+        });
   }
 
   private boolean isJB() {
